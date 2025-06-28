a3bee2353d86bd2245ed4f8fe354e57d8052806d
Thanh Le <thanhwaslike@gmail.com>
2025-06-28 02:15:43 -0700

    Added backend server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProductList from './ProductList';
+import ProductList from './productlist.jsx'; // Import the ProductList component
 
 function App() {
   return (
